270e8c2cf27ac3282ac40d934951f533e510fd4d
askesis <asketes@milkandcartoons.com>
2018-03-22 11:43:43 +0300

    Show books on home page

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,20 +3,22 @@ import { withTracker } from 'meteor/react-meteor-data';
 
 import Books from '../api/books'
 
+import Home from '../ui/components/Home';
+
 class App extends React.Component {
   render() {
-    console.log(this.props);
+    const { loading, books } = this.props;
     return (
-      <div>
-        <h1> Hi</h1>
-      </div>
+      <React.Fragment > 
+        { !loading && <Home books={books} /> }
+      </React.Fragment>
     );
   }
 }
 
 const AppWithTracker = withTracker(() => {
   const booksSubs = Meteor.subscribe('books.public');
-  const loading = booksSubs.ready();
+  const loading = !booksSubs.ready();
   const books = Books.find().fetch();
 
   return {
---
+++ imports/ui/components/Home.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+rendererList(){
+  const { books } = this.props;
+
+  return books && books.map( book => {
+    const { _id, name, author, description, range } = book;
+    return (
+      <React.Fragment key={_id}> 
+        <div>
+          <p>{name}</p>
+          <p>{author}</p>
+          <p>{description}</p>
+          <p>{range}</p>
+        </div>
+        <hr/>
+        <br/>
+      </React.Fragment> 
+    )
+  })
+}
+
+  render() {
+    return (
+      <React.Fragment> 
+        <h1>Library</h1>
+        <br/>
+        {this.rendererList()}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Home;
